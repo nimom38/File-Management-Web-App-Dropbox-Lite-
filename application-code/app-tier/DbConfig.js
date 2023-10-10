fb4ef7f8c340f29afcd73ae7051395b933444c4d
Gazi Momin Iqbal <gazimiqbal@gmail.com>
2023-10-10 10:22:23 -0700

    make local connection

--- application-code/app-tier/DbConfig.js
+++ application-code/app-tier/DbConfig.js
@@ -1,5 +1,5 @@
 module.exports = Object.freeze({
-  DB_HOST: "database-1.cluster-cayq6mvyhh3r.us-west-1.rds.amazonaws.com",
+  DB_HOST: "database-3-experiment.cayq6mvyhh3r.us-west-1.rds.amazonaws.com",
   DB_USER: "admin",
   DB_PWD: "admin123",
   DB_DATABASE: "webappdb",
