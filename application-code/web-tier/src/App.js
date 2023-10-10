2e1d0f9a48f6ebdcf7b89474eaabfca908e633d0
Gazi Momin Iqbal <gazimiqbal@gmail.com>
2023-10-10 11:14:07 -0700

    create authentication skeleton

--- application-code/web-tier/src/App.js
+++ application-code/web-tier/src/App.js
@@ -1,43 +1,28 @@
-import React, { useState, useRef } from "react";
-import { ThemeProvider } from "styled-components";
-import { useOnClickOutside } from "./hooks";
-import { GlobalStyles } from "./global";
-import { theme } from "./theme";
-import { Burger, Menu } from "./components";
-import FocusLock from "react-focus-lock";
-import DatabaseDemo from "./components/DatabaseDemo/DatabaseDemo";
+import React, { useState } from "react";
 import Home from "./components/Home/Home";
+import Login from "./components/Login/Login";
+import Register from "./components/Register/Register";
 import { HashRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
-  const [open, setOpen] = useState(false);
-  const node = useRef();
-  const menuId = "main-menu";
-
-  useOnClickOutside(node, () => setOpen(false));
+  const [user, setUser] = useState({ name: "asd" });
 
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyles />
-      <div ref={node}>
-        <FocusLock disabled={!open}>
-          <Burger open={open} setOpen={setOpen} aria-controls={menuId} />
-          <Router>
-            <div>
-              <Menu open={open} setOpen={setOpen} id={menuId} />
-              <Switch>
-                <Route path="/db">
-                  <DatabaseDemo />
-                </Route>
-                <Route path="/">
-                  <Home />
-                </Route>
-              </Switch>
-            </div>
-          </Router>
-        </FocusLock>
-      </div>
-    </ThemeProvider>
+    <div>
+      <Router>
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/register">
+            <Register />
+          </Route>
+          <Route path="/">
+            <Home user={user} />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
   );
 }
 
--- application-code/web-tier/src/components/Home/Home.js
+++ application-code/web-tier/src/components/Home/Home.js
@@ -1,16 +1,7 @@
+import React from "react";
 
-    import React, {Component} from 'react';
-    import architecture from '../../assets/3TierArch.png'
+function Home() {
+  return <div>Home Page</div>;
+}
 
-    class Home extends Component {
-        render () {
-        return (
-            <div>
-            <h1 style={{color:"white"}}>AWS 3-TIER WEB APP DEMO</h1>
-            <img src={architecture} alt="3T Web App Architecture" style={{height:400,width:825}} />
-          </div>
-        );
-      }
-    }
-
-    export default Home;
+export default Home;
