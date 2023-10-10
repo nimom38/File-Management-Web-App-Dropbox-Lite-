3e52c2a4b33fa8f16fe8d1ef4640868b1d2fabe2
Gazi Momin Iqbal <gazimiqbal@gmail.com>
2023-10-10 10:27:33 -0700

    install material ui

--- application-code/web-tier/src/App.js
+++ application-code/web-tier/src/App.js
@@ -1,18 +1,13 @@
-import React, { useState, useRef } from 'react';
-import { ThemeProvider } from 'styled-components';
-import { useOnClickOutside } from './hooks';
-import { GlobalStyles } from './global';
-import { theme } from './theme';
-import { Burger, Menu } from './components';
-import FocusLock from 'react-focus-lock';
-import DatabaseDemo from './components/DatabaseDemo/DatabaseDemo'
-import Home from './components/Home/Home'
-import {
-  HashRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
-
+import React, { useState, useRef } from "react";
+import { ThemeProvider } from "styled-components";
+import { useOnClickOutside } from "./hooks";
+import { GlobalStyles } from "./global";
+import { theme } from "./theme";
+import { Burger, Menu } from "./components";
+import FocusLock from "react-focus-lock";
+import DatabaseDemo from "./components/DatabaseDemo/DatabaseDemo";
+import Home from "./components/Home/Home";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   const [open, setOpen] = useState(false);
@@ -23,25 +18,25 @@ function App() {
 
   return (
     <ThemeProvider theme={theme}>
-        <GlobalStyles />
-        <div ref={node}>
-          <FocusLock disabled={!open}>
-            <Burger open={open} setOpen={setOpen} aria-controls={menuId} />
-            <Router>
-              <div>
-            <Menu open={open} setOpen={setOpen} id={menuId} />
-            <Switch>
-          <Route path="/db">
-            <DatabaseDemo />
-          </Route>
-          <Route path="/">
-            <Home/>
-          </Route>
-        </Switch>
-        </div>
-        </Router>
-          </FocusLock>
-        </div>
+      <GlobalStyles />
+      <div ref={node}>
+        <FocusLock disabled={!open}>
+          <Burger open={open} setOpen={setOpen} aria-controls={menuId} />
+          <Router>
+            <div>
+              <Menu open={open} setOpen={setOpen} id={menuId} />
+              <Switch>
+                <Route path="/db">
+                  <DatabaseDemo />
+                </Route>
+                <Route path="/">
+                  <Home />
+                </Route>
+              </Switch>
+            </div>
+          </Router>
+        </FocusLock>
+      </div>
     </ThemeProvider>
   );
 }
