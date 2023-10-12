c50cce35499c6158822e6e0473221b32202aa7f6
Gazi Momin Iqbal <gazimiqbal@gmail.com>
2023-10-12 00:29:25 -0700

    remove redundant codes

--- application-code/app-tier/DbConfig.js
+++
@@ -1,6 +0,0 @@
-module.exports = Object.freeze({
-  DB_HOST: "database-3-experiment.cayq6mvyhh3r.us-west-1.rds.amazonaws.com",
-  DB_USER: "admin",
-  DB_PWD: "admin123",
-  DB_DATABASE: "webappdb",
-});
